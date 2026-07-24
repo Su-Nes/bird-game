65ccefa7c6d6340139b5a4ba5c4b1d272b138095
Korl <ksuneps@gmail.com>
2026-07-24 18:40:39 +0300

    Fixed idle state velocity decay.

--- Player/ControllerStates/IdleState.gd
+++ Player/ControllerStates/IdleState.gd
@@ -7,6 +7,8 @@ class_name IdleState
 @onready var player_controller: CharacterBody3D = $"../.."
 @onready var walk_state: WalkState = $"../WalkState"
 
+var direction
+
 
 func enter():
 	print("Entering Idle state")
@@ -14,19 +16,25 @@ func enter():
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func physics_update(_delta):
-	walk_state.handle_velocity(_delta)
-	
+func physics_update(delta):	
 	# Switch to fall state if player controller is not on ground.
 	if !player_controller.is_on_floor():
 		state_machine.change_state("fallstate")
-	
-func handle_input(_event):
+		
+	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (player_controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction.length() > 0.0:
 		state_machine.change_state("walkstate")
 		
 	if Input.is_action_just_pressed("jump"):
 		state_machine.change_state("jumpstate")
+	
+	handle_velocity(delta)
+	
+func handle_velocity(delta):	
+	player_controller.velocity.x = lerp(player_controller.velocity.x, 0.0, delta * walk_state.GROUND_INERTIA)
+	player_controller.velocity.z = lerp(player_controller.velocity.z, 0.0, delta * walk_state.GROUND_INERTIA)
+	
+	player_controller.move_and_slide()
